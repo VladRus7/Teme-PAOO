98a49ab8662a3b0f1626843a4dbd1435dbc4fe6f
VladRus7 <vlad.rus777@yahoo.ro>
2024-12-12 17:50:59 +0200

    TEMA3

--- tema1/include/ReservationBase.hpp
+++ tema1/include/ReservationBase.hpp
@@ -1,27 +1,21 @@
 #pragma once
 #include <string>
 #include <iostream>
+#include <mutex>
 
 class ReservationBase {
 protected:
     std::string name;  // Numele terenului
     std::string type;  // Tipul terenului
-    double pricePerHour;  // Prețul pe oră al terenului
+    double pricePerHour;  // Pretul pe oră al terenului
+
+    static std::mutex mtx; // Mutex pt gestionarea resurselor
 
 public:
-    // Constructorul clasei de bază
-    ReservationBase(const std::string& name, const std::string& type, double pricePerHour)
-        : name(name), type(type), pricePerHour(pricePerHour) {
-        std::cout << "Obiectul ReservationBase a fost creat pentru "<< name  << std::endl;
-    }
+    ReservationBase(const std::string& name, const std::string& type, double pricePerHour);
+    virtual ~ReservationBase();
 
-    // Destructorul clasei de bază
-    virtual ~ReservationBase() {
-        std::cout << "Destructorul ReservationBase a fost apelat." << std::endl;
-    }
-
-    // Gettere
-    std::string getName() const { return name; }
-    std::string getType() const { return type; }
-    double getPricePerHour() const { return pricePerHour; }
+    std::string getName() const;
+    std::string getType() const;
+    double getPricePerHour() const;
 };
